85ce8e7a8113db68ccdf2b402743c16796855781
adrie-stclair <adrie.stclair@gmail.com>
2025-03-20 23:41:02 -0700

    onward

--- app.R
+++ app.R
@@ -1,20 +1,83 @@
+
+
 library(shiny)
-library(ggplot2)
+library(vroom)
+library(tidyverse)
+library(here)
+
+#4.6 Rate vs count
+
+dir.create(here("neiss"))
+
+#> Warning in dir.create("neiss"): 'neiss' already exists
+download <- function(name) {
+  url <- "https://raw.github.com/hadley/mastering-shiny/main/neiss/"
+  download.file(paste0(url, name), paste0("neiss/", name), quiet = TRUE)
+}
+
+download("injuries.tsv.gz")
+download("population.tsv")
+download("products.tsv")
+
+injuries <- vroom::vroom("neiss/injuries.tsv.gz")
+injuries
+
+products <- vroom::vroom("neiss/products.tsv")
+products
+
+population <- vroom::vroom("neiss/population.tsv")
+population
+
+selected <- injuries %>% filter(prod_code == 649)
+nrow(selected)
+#> [1] 2993
+
+count_top <- function(df, var, n = 5) {
+  df %>%
+    mutate({{ var }} := fct_lump(fct_infreq({{ var }}), n = n)) %>%
+    group_by({{ var }}) %>%
+    summarise(n = as.integer(sum(weight)))
+}
+
+
+prod_codes <- setNames(products$prod_code, products$title)
 
 ui <- fluidPage(
-  sliderInput(
-  "howmany", 
-  "How many?",
-  min = 0,
-  max = 100, 
-  step = 5,
-  value = 15,
-  animate = TRUE
+  fluidRow(
+    column(6,
+           selectInput("code", "Product", choices = prod_codes)
+    )
+  ),
+  fluidRow(
+    column(4, tableOutput("diag")),
+    column(4, tableOutput("body_part")),
+    column(4, tableOutput("location"))
+  ),
+  fluidRow(
+    column(12, plotOutput("age_sex"))
   )
 )
 
 server <- function(input, output, session) {
+  selected <- reactive(injuries %>% filter(prod_code == input$code))
   
+  output$diag <- renderTable(count_top(selected(), diag), width = "100%")
+  output$body_part <- renderTable(count_top(selected(), body_part), width = "100%")
+  output$location <- renderTable(count_top(selected(), location), width = "100%")
+  
+  summary <- reactive({
+    selected() %>%
+      count(age, sex, wt = weight) %>%
+      left_join(population, by = c("age", "sex")) %>%
+      mutate(rate = n / population * 1e4)
+  })
+  
+  output$age_sex <- renderPlot({
+    summary() %>%
+      ggplot(aes(age, n, colour = sex)) +
+      geom_line() +
+      labs(y = "Estimated number of injuries")
+  }, res = 96)
 }
 
 shinyApp(ui, server)
